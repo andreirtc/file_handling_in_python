fec329138b4b287959db1aba27c540962bc718ba
andreirtc <ralphandreicastillo326@gmail.com>
2024-04-21 23:05:23 +0800

    Check if numbers in the text file is even or odd

--- reading_a_text_file_with_integers_and_extracting_even_and_odd_numbers.py
+++ reading_a_text_file_with_integers_and_extracting_even_and_odd_numbers.py
@@ -5,6 +5,8 @@
 with open("numbers.txt", "r") as file:
     for num in file:
         numbers = int(num.strip())
-# 3. Check if even or odd number    
+# 3. Check if even or odd number 
+even_numbers = (num for num in numbers if num % 2 == 0)
+odd_numbers = (num for num in numbers if  num % 2 != 0)
 # 4. Extract the even numbers from the text file and name it "even.txt"
 # 5. Extract the odd numbers from the text file and name it "odd.txt"
