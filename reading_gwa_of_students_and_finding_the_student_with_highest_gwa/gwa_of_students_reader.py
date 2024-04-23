df8008c87f69125313da687286f43a1d915291a4
andreirtc <ralphandreicastillo326@gmail.com>
2024-04-23 18:28:30 +0800

    Pseudocode

---
+++ reading_gwa_of_students_and_finding_the_student_with_highest_gwa/gwa_of_students_reader.py
@@ -0,0 +1,6 @@
+# Pseudocode
+
+# 1. Create a text file with students' name and GWA
+# 2. Create an empty list of student data, read student data from the text file, split the name and GWA, and set 1.00 as highest and 5.00 as lowest GWA
+# 3. Initialize highest student name and GWA as None and 0 and find the student with highest GWA
+# 4. Print the student with the highest GWA
