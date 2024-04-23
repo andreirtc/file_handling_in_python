822b40ce9271addbc3b983d91d2ffb7e425892f0
andreirtc <ralphandreicastillo326@gmail.com>
2024-04-23 19:22:26 +0800

    Changed some example integers and read the integers.txt file

--- finding_the_squares_and_cubes_of_integers_inside_a_text_file/reading_integers_and_finding_their_squares_and_cubes.py
+++ finding_the_squares_and_cubes_of_integers_inside_a_text_file/reading_integers_and_finding_their_squares_and_cubes.py
@@ -1,7 +1,11 @@
 # Pseudocode
 
 # 1. Create a text file named "integers.txt" that contains 20 integers
+
 # 2. Read integers from the text file and convert them as integers
+with open("finding_the_squares_and_cubes_of_integers_inside_a_text_file/integers.txt", "r") as file:
+    numbers = [int(num) for num in file.read().split()]
+
 # 3. Square even integers and cube odd integers
 # 4. Create the file for the square of even integers and name it 'double.txt'
 # 5. Create the file for the cube of odd integers and name it 'triple.txt'
