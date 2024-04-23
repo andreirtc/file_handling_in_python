d4b982dd6530f8cd525fb5fe3a8e00c5c3df0b4a
andreirtc <ralphandreicastillo326@gmail.com>
2024-04-23 19:14:07 +0800

    Pseudocode

---
+++ finding_the_squares_and_cubes_of_integers_inside_a_text_file/reading_integers_and_finding_their_squares_and_cubes.py
@@ -0,0 +1,7 @@
+# Pseudocode
+
+# 1. Create a text file named "integers.txt" that contains 20 integers
+# 2. Read integers from the text file and convert them as integers
+# 3. Square even integers and cube odd integers
+# 4. Create the file for the square of even integers and name it 'double.txt'
+# 5. Create the file for the cube of odd integers and name it 'triple.txt'
