e0f06d58bce74de7d22e04a9b62d9e99599d51c0
andreirtc <ralphandreicastillo326@gmail.com>
2024-04-23 17:19:27 +0800

    Pseudocode

---
+++ extracting_even_and_odd_numbers_from_a_text_file/reading_a_text_file_with_integers_and_extracting_even_and_odd_numbers.py
@@ -0,0 +1,7 @@
+# Pseudocode
+
+# 1. Create a text file named "numbers.txt" that contains 20 integers
+# 2. Read the text file and convert the content as integer
+# 3. Check the content if even or odd number
+# 4. Extract the even numbers from the text file and name it "even.txt"
+# 5. Extract the odd numbers from the text file and name it "odd.txt"
