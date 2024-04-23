decfb35c089b491c5b12e5086fab9aa147abf9cd
andreirtc <ralphandreicastillo326@gmail.com>
2024-04-23 19:37:58 +0800

    Creating file for square of even integers and cube of odd integers

--- finding_the_squares_and_cubes_of_integers_inside_a_text_file/reading_integers_and_finding_their_squares_and_cubes.py
+++ finding_the_squares_and_cubes_of_integers_inside_a_text_file/reading_integers_and_finding_their_squares_and_cubes.py
@@ -9,5 +9,13 @@ with open("finding_the_squares_and_cubes_of_integers_inside_a_text_file/integers
 # 3. Square even integers and cube odd integers
 even_numbers = [num ** 2 for num in numbers if num % 2 == 0]
 odd_numbers = [num ** 3 for num in numbers if num % 2 != 0]
+
 # 4. Create the file for the square of even integers and name it 'double.txt'
+with open("finding_the_squares_and_cubes_of_integers_inside_a_text_file/double.txt", "w") as double_file:
+    for num in even_numbers:
+        double_file.write(str(num) + "\n")
+
 # 5. Create the file for the cube of odd integers and name it 'triple.txt'
+with open("finding_the_squares_and_cubes_of_integers_inside_a_text_file/triple.txt", "w") as triple_file:
+    for num in odd_numbers:
+        triple_file.write(str(num) + "\n")
