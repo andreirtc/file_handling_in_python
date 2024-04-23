7e97794c059ff1abfd318de5d50b9bab68a68d09
andreirtc <ralphandreicastillo326@gmail.com>
2024-04-23 18:54:07 +0800

    Pseudocode

---
+++ writing_multiple_line_of_texts_into_a_text_file/text_lines_saver.py
@@ -0,0 +1,5 @@
+# Pseudocode
+
+# 1. Create a text file named "my_life.txt" using 'write' in python.
+# 2. Use while loop to ask the user repeatedly about the lines to enter and break if the user entered 'n'
+# 3. Display a text indicating the texts inputted was written to 'my_life.txt' successfully
