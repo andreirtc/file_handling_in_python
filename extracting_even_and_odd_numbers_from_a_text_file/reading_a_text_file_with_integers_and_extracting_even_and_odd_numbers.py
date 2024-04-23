5c368e0f3f51f20d4f258e7b76c4890869176e56
andreirtc <ralphandreicastillo326@gmail.com>
2024-04-23 17:26:41 +0800

    Read the text file and convert the content as integer

--- extracting_even_and_odd_numbers_from_a_text_file/reading_a_text_file_with_integers_and_extracting_even_and_odd_numbers.py
+++ extracting_even_and_odd_numbers_from_a_text_file/reading_a_text_file_with_integers_and_extracting_even_and_odd_numbers.py
@@ -1,7 +1,11 @@
 # Pseudocode
 
 # 1. Create a text file named "numbers.txt" that contains 20 integers
+
 # 2. Read the text file and convert the content as integer
+with open("numbers.txt", "r") as file:
+    numbers = [int(num.strip()) for num in file.readlines()]
+    
 # 3. Check the content if even or odd number
 # 4. Extract the even numbers from the text file and name it "even.txt"
 # 5. Extract the odd numbers from the text file and name it "odd.txt"
